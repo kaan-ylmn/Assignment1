20cb1eeab87a0233ee73ae7a25cdf65cbf18b645
Kaan Yalman <kaan.yalman6@gmail.com>
2026-04-29 22:00:37 +0300

    Fixed AIController issues and completed BaseEnemyShip

---
+++ Source/BirdOfPrey/Private/BaseEnemyShip.cpp
@@ -0,0 +1,12 @@
+#include "BaseEnemyShip.h"
+#include "AIController.h"
+
+void ABaseEnemyShip::Move()
+{
+}
+
+void ABaseEnemyShip::Reset()
+{
+	
+	Super::Reset();
+}
---
+++ Source/BirdOfPrey/Public/BaseEnemyShip.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BaseShip.h"
+#include "BaseEnemyShip.generated.h"
+
+UCLASS()
+class BIRDOFPREY_API ABaseEnemyShip : public ABaseShip
+{
+	GENERATED_BODY()
+
+public:
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "BirdOfPrey")
+	class AAIController* ShipAIController;
+
+	UFUNCTION(BlueprintCallable, Category = "BirdOfPrey")
+	void Move();
+
+	
+	virtual void Reset() override;
+};
